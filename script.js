379cb5a5850f37fc3ae44c1a64a6d0c8c55c8301
nathank7256 <nathank7256@gmail.com>
2022-09-28 16:41:50 -0500

    Created Board factory that can create a grid of any size, and tracks wins/losses/ties

--- script.js
+++ script.js
@@ -1,46 +1,90 @@
-const board = document.querySelector('#board');
-const square = board.querySelectorAll('.square');
-let gameBoard = ["", "", "",
-                 "", "", "",
-                 "", "", "",];
+const boardDiv = document.querySelector('#board');
+const square = boardDiv.querySelectorAll('.square');
 let player1Move = true;
-let player1 = "";
-let player2 = "";
-square.forEach((square, squareIndex) => {
-    square.addEventListener('click', () => {
-        console.log(squareIndex);
-        if(gameBoard[squareIndex] === ""){
-            square.innerHTML = `${(player1Move ? "x" : "o")}`;
-            gameBoard[squareIndex] = `${(player1Move ? "x" : "o")}`;
-            player1Move = !player1Move
-            console.log("gameboard" + gameBoard + ` win is ${checkForWin()}`)
-            console.log(checkForWin())
-        }
-    });
-});
 
-function checkForWin(){
-    populated = 0;
-    rowcount = Math.sqrt(gameBoard.length)
-    gameBoard2D = [];
-    for(let i = 0; i < gameBoard.length; i += rowcount){
-        const row = gameBoard.slice(i, i + rowcount);
-        gameBoard2D.push(row)
-        console.log(gameBoard2D);
-    }
-    gameBoard2D.forEach(row => {
-        if(row.every(square => square === "x") || row.every(square => square === "o")){
-            console.log("bazinga");
-            return true;
+const Board = (gridSize) => {
+    function createBoard(){
+        let boardArr = [];
+        for(let i = 0; i < (gridSize * gridSize); i++){
+            const square = document.createElement('div');
+            square.classList.add("square");
+            square.addEventListener('click', () => {
+                if(square.innerText === ""){
+                    square.innerHTML = `${(player1Move ? "x" : "o")}`;
+                    player1Move = !player1Move;
+                    board.checkForWin();
+                }
+            });
+            boardDiv.append(square)
+            boardArr.push(square);
         }
-    })
-    if(populated < rowcount){
-        return false;
+        return boardArr;
     }
-    else{
-        return true;
+    let gameBoard = createBoard(gridSize);
+    const rowCoordinates = () => {
+        let gameBoard2D = [];
+        for(let i = 0; i < gameBoard.length; i += gridSize){
+            const row = gameBoard.slice(i, i + gridSize);
+            gameBoard2D.push(row)
+        }
+        return gameBoard2D;
     }
+    const columnCoordinates = () => {
+        let gameBoard2D = rowCoordinates();
+        let gameBoardColumn = [];
+        let gameBoardColumns = [];
+        for(let i = 0; i < gameBoard2D.length; i += 1){
+            for(let j = 0; j < gameBoard2D.length; j += 1){
+                gameBoardColumn.push(gameBoard2D[j][i]);
+                if(gameBoardColumn.length === gridSize){
+                    gameBoardColumns.push(gameBoardColumn)
+                    gameBoardColumn = [];
+                }
+            }
+        }
+        return gameBoardColumns;
+    }
+    const lDiagCoordinates = () => {
+        let gameBoard2D = rowCoordinates();
+        let gameBoardDiags = [];
+        for(let i = 0; i < gameBoard2D.length; i += 1){
+            gameBoardDiags.push(gameBoard2D[i][i]);
+
+        }
+        return gameBoardDiags;
+    }
+    const rDiagCoordinates = () => {
+        let gameBoard2D = rowCoordinates();
+        let gameBoardDiags = [];
+        let j = 0;
+        for(let i = gridSize - 1; i >= 0; i -= 1){
+            gameBoardDiags.push(gameBoard2D[i][j])
+            j+=1;
+        }
+        return gameBoardDiags;
+    }
+    const checkForWin = () => {
+        board.rowCoordinates().forEach(row => {
+            if(row.every(square => square.innerText === "x") || row.every(square => square.innerText === "o")){
+                console.log(row[0].innerHTML + " wins!");
+            }
+        });
+        board.columnCoordinates().forEach(column => {
+            if(column.every(square => square.innerText === "x") || column.every(square => square.innerText === "o")){
+                console.log(column[0].innerHTML + " wins!");
+            }
+        });
+        if(lDiagCoordinates().every(square => square.innerText === "x") || lDiagCoordinates().every(square => square.innerText === "o")){
+            console.log(lDiagCoordinates()[0].innerHTML + " wins!");
+        }
+        if(rDiagCoordinates().every(square => square.innerText === "x") || rDiagCoordinates().every(square => square.innerText === "o")){
+            console.log(rDiagCoordinates()[0].innerHTML + " wins!");
+        }
+        if(gameBoard.every(square => square.innerText !== "")){
+            console.log("Tie");
+        }
+    }
+    return {createBoard, rowCoordinates, columnCoordinates, lDiagCoordinates, rDiagCoordinates, checkForWin}
 }
-console.log(Math.sqrt(gameBoard.length));
-console.log(checkForWin());
-console.log(["x", "x", "x"].every(square => square === "x"));
+
+const board = Board(3);
